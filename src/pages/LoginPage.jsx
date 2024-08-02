ccf78d59723ab4d1a472c22109486953dd1b4852
mfsohail12 <149211499+mfsohail12@users.noreply.github.com>
2024-08-02 07:50:17 -0400

    modified sign up page and created login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,48 @@
+import { NavLink } from "react-router-dom";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div
+        id="loginBox"
+        className="w-[375px] h-[425px] shadow-lg flex flex-col items-center"
+      >
+        <NavLink to="/">
+          <h1 className="text-sky-500 text-2xl font-bold mt-9">
+            Production Connect
+          </h1>
+        </NavLink>
+        <form id="signUpForm" className="w-5/6 flex flex-col gap-5 mt-7">
+          <label htmlFor="email" className="w-full">
+            Email Address
+            <input
+              id="email"
+              type="text"
+              className="border-2 block w-full indent-1.5 p-[2px] mt-1"
+            />
+          </label>
+          <label htmlFor="password" className="w-full">
+            Password
+            <input
+              id="password"
+              type="password"
+              className="border-2 block w-full indent-1.5 p-[2px] mt-1"
+            />
+          </label>
+
+          <button className="w-full mt-2 h-9 bg-sky-500 rounded-lg text-white font-bold">
+            Login
+          </button>
+        </form>
+        <p className="text-sm mt-5">
+          Don't have an account yet?{" "}
+          <NavLink to="/sign-up" className="text-sky-500">
+            Sign up
+          </NavLink>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import logo from "../assets/logo.png";
+import { useNavigate, NavLink } from "react-router-dom";
 
 const SignUpPage = () => {
   const [clientActive, setClientActive] = useState(true);
-  const [editorActive, setEditorActive] = useState(false);
   const [formData, setFormData] = useState({
     accountType: "client",
     firstName: "",
@@ -12,20 +10,19 @@ const SignUpPage = () => {
     email: "",
     password: "",
   });
+  const [confirmPass, setConfirmPass] = useState("");
 
   const navigate = useNavigate();
 
   // Handles activating account type button depending on which account type button is clicked
   function activateBtn(event) {
-    if (event.target.id === "clientBtn" && editorActive) {
+    if (event.target.id === "clientBtn" && !clientActive) {
       setClientActive(true);
-      setEditorActive(false);
       setFormData((prevData) => ({
         ...prevData,
         accountType: "client",
       }));
     } else if (event.target.id === "editorBtn" && clientActive) {
-      setEditorActive(true);
       setClientActive(false);
       setFormData((prevData) => ({
         ...prevData,
@@ -34,15 +31,12 @@ const SignUpPage = () => {
     }
   }
 
-  // Sets client button class depending on if it is currently active
-  // Note: DRY
+  // Sets client/editor button class depending on if it is currently active
   const clientBtnClass = clientActive
     ? "h-7 w-44 border-2 rounded-lg border-sky-500 bg-sky-500 text-white"
     : "h-7 w-44 border-2 rounded-lg border-sky-500";
 
-  // Sets editor button class depending on if it is currently active
-  // Note: DRY
-  const editorBtnClass = editorActive
+  const editorBtnClass = !clientActive
     ? "h-7 w-44 border-2 rounded-lg border-sky-500 bg-sky-500 text-white"
     : "h-7 w-44 border-2 rounded-lg border-sky-500";
 
@@ -50,16 +44,17 @@ const SignUpPage = () => {
   function handleChange(event) {
     const { name, value } = event.target;
 
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+    name === "confirmPassword"
+      ? setConfirmPass(value)
+      : setFormData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
   }
 
   // Handles form submission when sign up button is clicked
   function handleSubmit(event) {
     event.preventDefault();
-    const confirmPass = document.getElementById("confirmPassword").value;
 
     if (formData.password === confirmPass) {
       return navigate("/login");
@@ -72,9 +67,13 @@ const SignUpPage = () => {
     <div className="h-screen w-screen flex justify-center items-center">
       <div
         id="signUpBox"
-        className="w-[450px] h-2/3 shadow-lg flex flex-col items-center"
+        className="w-[450px] h-[550px] shadow-lg flex flex-col items-center"
       >
-        <img src={logo} alt="Production Connect logo" className="w-60" />
+        <NavLink to="/">
+          <h1 className="text-sky-500 text-2xl font-bold mt-9">
+            Production Connect
+          </h1>
+        </NavLink>
         <div
           id="accountTypeSelector"
           className="w-5/6 flex justify-between my-7 text-sky-500"
@@ -96,30 +95,30 @@ const SignUpPage = () => {
         </div>
         <form
           id="signUpForm"
-          className="w-5/6 flex flex-col items-center gap-5"
+          className="flex flex-col items-center gap-5"
           onSubmit={handleSubmit}
         >
           <span className="flex gap-3">
-            <label htmlFor="firstName" className="">
+            <label htmlFor="firstName">
               First Name
               <input
                 id="firstName"
                 type="text"
                 required
-                className="border-2 block"
+                className="border-2 block indent-1.5 p-[2px] mt-1"
                 onChange={handleChange}
                 name="firstName"
                 value={formData.firstName}
               />
             </label>
 
-            <label htmlFor="lastName" className="">
+            <label htmlFor="lastName">
               Last Name
               <input
                 id="lastName"
                 type="text"
                 required
-                className="border-2 block"
+                className="border-2 block indent-1.5 p-[2px] mt-1"
                 onChange={handleChange}
                 name="lastName"
                 value={formData.lastName}
@@ -133,7 +132,7 @@ const SignUpPage = () => {
               id="email"
               type="email"
               required
-              className="border-2 block w-full"
+              className="border-2 block w-full indent-1.5 p-[2px] mt-1"
               onChange={handleChange}
               name="email"
               value={formData.email}
@@ -141,13 +140,13 @@ const SignUpPage = () => {
           </label>
 
           <span className="flex gap-3">
-            <label htmlFor="password" className="">
+            <label htmlFor="password">
               Password
               <input
                 id="password"
                 type="password"
                 required
-                className="border-2 block"
+                className="border-2 block indent-1.5 p-[2px] mt-1"
                 onChange={handleChange}
                 name="password"
                 value={formData.password}
@@ -160,14 +159,23 @@ const SignUpPage = () => {
                 type="password"
                 id="confirmPassword"
                 required
-                className="border-2 block"
+                className="border-2 block indent-1.5 p-[2px] mt-1"
+                name="confirmPassword"
+                value={confirmPass}
+                onChange={handleChange}
               />
             </label>
           </span>
-          <button className="w-32 h-9 bg-sky-500 rounded-lg text-white font-bold">
+          <button className="w-full mt-5 h-9 bg-sky-500 rounded-lg text-white font-bold">
             Sign up
           </button>
         </form>
+        <p className="text-sm mt-5">
+          Already have an account?{" "}
+          <NavLink to="/login" className="text-sky-500">
+            Login
+          </NavLink>
+        </p>
       </div>
     </div>
   );
