f1c361572496ca8e67707e3cf96557011fb8af9e
mfsohail12 <149211499+mfsohail12@users.noreply.github.com>
2024-10-15 15:42:18 -0400

    added footer to landing page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,19 @@
 import Hero from "../components/Hero";
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <footer className="bg-black h-10 flex justify-center items-center relative px-4">
+        <Link to="https://github.com/mfsohail12/production-connect">
+          <FaGithub className="text-violet-600 text-xl" />
+        </Link>
+        <p className="text-white text-xs ml-auto">© 2024</p>
+      </footer>
+    </>
+  );
 };
 
 export default HomePage;
