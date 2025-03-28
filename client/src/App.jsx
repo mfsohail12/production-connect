ba46fa87a75034880561fb5d0cf7eeb9434e05dd
mfsohail12 <149211499+mfsohail12@users.noreply.github.com>
2025-03-28 04:06:16 -0400

    vercel analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
 import axios from "axios";
 import { UserProvider } from "./context/userContext";
 import AppRouter from "./routes/AppRouter";
+import { Analytics } from "@vercel/analytics/react";
 
 axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
   return (
-    <UserProvider>
-      <AppRouter />
-    </UserProvider>
+    <>
+      <UserProvider>
+        <AppRouter />
+      </UserProvider>
+      <Analytics />
+    </>
   );
 }
 
