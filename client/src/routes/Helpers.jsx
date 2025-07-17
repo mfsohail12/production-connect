03bef5e94301a76f9053cb668d6ad0096191edd2
mfsohail12 <149211499+mfsohail12@users.noreply.github.com>
2025-07-16 22:31:57 -0400

    added auth helpers

--- client/src/routes/Helpers.jsx
+++ client/src/routes/Helpers.jsx
@@ -1,7 +1,9 @@
 import { redirect } from "react-router-dom";
+import { getCookie } from "../helpers/cookieHelper";
 
 export const isAuthenticated = async () => {
-  const token = localStorage.getItem("token");
-  if (token) throw redirect("/dashboard");
+  const cookie = getCookie("token");
+  const localCookie = localStorage.getItem("token");
+  if (cookie && localCookie) throw redirect("/dashboard");
   return null;
 };
